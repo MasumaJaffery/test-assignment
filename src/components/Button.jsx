1a9be1bd288485e0bae1530e394a18581efb76bb
MasumaJaffery <masumajaffery1@gmail.com>
2024-04-01 16:42:39 +0500

    Intial UI commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+const MyButton = () => {
+  return (
+    <Button variant="contained" color="primary">
+      Hello Material-UI Button
+    </Button>
+  );
+};
+
+export default MyButton;
